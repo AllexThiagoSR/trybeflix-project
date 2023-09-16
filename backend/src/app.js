8c0effd3a77cab542c6e7697fd0d979c057d49fe
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-09-16 20:02:00 -0300

    Configuração do app

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,4 +8,4 @@ app.get('/', (_request, response) => {
   response.status(200).json({ message: 'OK' });
 });
 
-app.listen(process.env.PORT || 3001, () => console.log('API is running'));
+module.exports = app;

e4664f340b3aa50d5c606d27c00ba1ae27df54cf
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-09-17 17:00:30 -0300

    Inclui as tabelas de diretores e gêneros na query de filmes

--- backend/src/models/movies.model.js
+++ backend/src/models/movies.model.js
@@ -2,7 +2,18 @@ const connection = require('./connection');
 
 const getById = async (id) => {
   const [[movie]] = await connection.execute(
-    'SELECT * FROM movies WHERE id = ?',
+    `
+    SELECT
+      movies.id AS id,
+      movies.name AS name,
+      directors.name AS director,
+      release_year AS releaseYear,
+      genres.name AS genre
+    FROM movies
+    INNER JOIN directors ON director_id = directors.id
+    INNER JOIN genres ON genre_id = genres.id
+    WHERE movies.id = ?;
+    `,
     [id],
   );
   return movie;

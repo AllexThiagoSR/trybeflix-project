91d9acf3db74110bb1182253bb0fe76b8f8470cd
Vitor Nogueira <vslnogueira96@gmail.com>
2023-09-19 14:51:53 -0300

    feat: creating functions to retrieve all directors and genres

--- backend/src/models/movies.model.js
+++ backend/src/models/movies.model.js
@@ -1,11 +1,27 @@
 const connection = require('./connection');
-const { GET_ALL_MOVIES_QUERY } = require('./querys');
+const {
+  GET_ALL_MOVIES_QUERY,
+  GET_ALL_DIRECTORS_QUERY,
+  GET_ALL_GENRES_QUERY,
+} = require('./querys');
 
 const getAll = async () => {
   const [movies] = await connection.execute(GET_ALL_MOVIES_QUERY);
   return movies;
 };
 
+const getDirectors = async () => {
+  const [directors] = await connection.execute(GET_ALL_DIRECTORS_QUERY);
+  return directors;
+};
+
+const getGenres = async () => {
+  const [genres] = await connection.execute(GET_ALL_GENRES_QUERY);
+  return genres;
+};
+
 module.exports = {
   getAll,
+  getDirectors,
+  getGenres,
 };

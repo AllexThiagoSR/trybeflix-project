40ab496a249765dc880fb4f6cbc70ed190d904f0
Vitor Nogueira <vslnogueira96@gmail.com>
2023-09-19 14:55:44 -0300

    feat: creating function in service to create a new film and implementing director and genre validation

--- backend/src/services/movie.service.js
+++ backend/src/services/movie.service.js
@@ -1,10 +1,37 @@
 const { movieModel } = require('../models');
+const {
+  validateDirector,
+  validateGenre,
+} = require('./validations/validations');
 
 const getAll = async () => {
   const movies = await movieModel.getAll();
   return movies;
 };
 
+const createMovie = async (name, release_year, director_id, genre_id) => {
+  try {
+    await Promise.all([validateDirector(director_id), validateGenre(genre_id)]);
+
+    const movie = await movieModel.createMovie(
+      name,
+      release_year,
+      director_id,
+      genre_id
+    );
+    return {
+      error: false,
+      message: movie,
+    };
+  } catch (error) {
+    return {
+      error: true,
+      message: error.message,
+    };
+  }
+};
+
 module.exports = {
   getAll,
+  createMovie,
 };

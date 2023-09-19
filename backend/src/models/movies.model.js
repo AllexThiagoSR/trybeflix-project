2f0650ff66d464f45a9920c98c131b469e5444a5
Vitor Nogueira <vslnogueira96@gmail.com>
2023-09-19 14:49:15 -0300

    feat: creating function in the model to retrieve all movies

--- backend/src/models/movies.model.js
+++ backend/src/models/movies.model.js
@@ -1,11 +1,10 @@
 const connection = require('./connection');
+const { GET_ALL_MOVIES_QUERY } = require('./querys');
 
 const getAll = async () => {
-  const [movies] = await connection.execute(
-    'SELECT * FROM movies',
-  );
+  const [movies] = await connection.execute(GET_ALL_MOVIES_QUERY);
   return movies;
-}
+};
 
 module.exports = {
   getAll,
